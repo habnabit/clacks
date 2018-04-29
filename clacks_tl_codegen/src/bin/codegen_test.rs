0672f003433e7d38a84119b9e51f926ab7271836
Aaron Gallagher <_@habnab.it>
2018-04-29 14:39:33 -0700

    Rename a last stray thing.

--- clacks_tl_codegen/src/bin/codegen_test.rs
+++ clacks_tl_codegen/src/bin/codegen_test.rs
@@ -1,9 +1,9 @@
-extern crate tl_codegen;
+extern crate clacks_tl_codegen;
 
 use std::io::Read;
 
 fn main() {
     let mut input = String::new();
     std::io::stdin().read_to_string(&mut input).unwrap();
-    println!("{}", tl_codegen::generate_code_for(&input));
+    println!("{}", clacks_tl_codegen::generate_code_for(&input));
 }
